2e5358b981ae7e462084415d7023f6d589c87363
Fatima Zohra Bakali <bakalifatimazahrae87@gmail.com>
2023-05-15 00:08:27 +0200

    modifications sur l'affichage

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -4,9 +4,12 @@ namespace App\Form;
 
 use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 
 
 class ProduitType extends AbstractType
@@ -15,9 +18,28 @@ class ProduitType extends AbstractType
     {
         $builder
             ->add('nom')
-            ->add('prix')
+            ->add('prix', MoneyType::class, [
+                    'attr' => [
+                        'class' => 'form-control',
+                    ],
+                    'label' => 'Prix ',
+                    'label_attr' => [
+                        'class' => 'form-label mt-4'
+                    ],
+            ])
             ->add('categorie_id')
-            ->add('ordonnance')
+            ->add('ordonnance', CheckboxType::class, [
+                'label' => 'Ordonnance',
+                'attr' => [
+                    'class' => 'form-check-input',
+                ],
+                'label_attr' => [
+                    'class' => 'form-check-label'
+                ],
+                'constraints' => [
+                    new Assert\NotNull()
+                ]
+            ])
             ->add('imageFile', VichImageType::class, [
                 'label' => 'Image du produit',
                 'label_attr' => [
