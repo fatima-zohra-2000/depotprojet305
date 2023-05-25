958ff25953dde535e17e9a951773d5d2daacad8d
Fatima Zohra Bakali <bakalifatimazahrae87@gmail.com>
2023-05-25 05:00:28 +0200

    l'ajout de la fonction de création du rapport de la commande en pdf et la possibilité de le télécharger (CommandeController) + la view twig qui gère l'affichage du rapport (rapports/commande.html.twig

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -8,9 +8,11 @@ use App\Entity\TailleCommande;
 use App\Entity\Produit;
 use App\Repository\CommandeRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Dompdf\Dompdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/commande')]
@@ -105,4 +107,63 @@ class CommandeController extends AbstractController
 
         return $this->redirectToRoute('app_commande_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    #[Route('/{id}/rapport', name: 'app_commande_rapport')]
+    public function rapportCommande(Commande $commande): Response
+    {
+        $html = $this->renderView('rapports/commande.html.twig', ['commande' => $commande]);
+
+        return new Response($html);
+    }
+
+    #[Route('/{id}/rapport/download', name: 'app_commande_rapport_download')]
+    public function rapportCommandeDownload(Commande $commande): Response
+    {
+        //la fonction convertit la page html en pdf et permet de la télécharger (c'est fonctionnel)
+
+        $html = $this->renderView('rapports/commande.html.twig', ['commande' => $commande]);
+
+        $filename = 'rapport_commande_' . $commande->getId() . '.pdf';
+
+        // Conversion du contenu HTML en PDF
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A5', 'portrait');
+        $dompdf->render();
+        $pdfContent = $dompdf->output();
+
+        // Téléchargement du rapport PDF
+        $response = new Response($pdfContent);
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Content-Disposition', $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $filename
+        ));
+
+        return $response;
+    }
+
+    // La fonction qui vient est à voir si la fonction précédente n'est pas fonctionnelle avec l'application de bureau
+
+//    #[Route('/{id}/rapport', name: 'app_commande_rapport')]
+//    public function rapportCommande(Commande $commande): Response
+//    {
+//        $html = $this->renderView('rapports/commande.html.twig', ['commande' => $commande]);
+//
+//        // Conversion du contenu HTML en PDF
+//        $dompdf = new Dompdf();
+//        $dompdf->loadHtml($html);
+//        $dompdf->setPaper('A4', 'portrait');
+//        $dompdf->render();
+//        $pdfContent = $dompdf->output();
+//
+//        // Enregistrement du fichier PDF localement
+//        $filename = 'rapport_commande_' . $commande->getId() . '.pdf';
+//        $filePath = '/chemin/vers/dossier/rapports/' . $filename;
+//        file_put_contents($filePath, $pdfContent);
+//
+//        // Redirection vers la page de visualisation du rapport
+//        return $this->redirectToRoute('app_commande_index', ['filename' => $filename]);
+//    }
+
 }
